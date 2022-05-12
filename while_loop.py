6b8283880e195e9b4d0a6c5627ae73cb89ccdf92
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2022-05-12 23:26:01 +0600

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+x = 2
+
+"""while x < 10:
+    print("x value", x)
+    x += 1"""
+
+"""while False: #not working
+    print("x value", x)
+    x += 1"""
+
+"""while True: #continusly working
+    print("x value", x)
+    x +=1
+"""
+while True:
+    print("x value:", x)
+    if x == 100:
+        break
+    x += 1
+    
+        
